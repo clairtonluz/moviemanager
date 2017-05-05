713754c4a66097ae004f6103671264b940bc0222
clairtonluz <clairton.c.l@gmail.com>
2017-05-04 21:21:18 -0300

    setup cors

--- src/main/java/br/com/clairtonluz/api/security/LoginAPI.java
+++ src/main/java/br/com/clairtonluz/api/security/LoginAPI.java
@@ -3,11 +3,13 @@ package br.com.clairtonluz.api.security;
 import br.com.clairtonluz.model.entity.security.User;
 import br.com.clairtonluz.service.security.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
+@CrossOrigin(origins = "*")
 @RestController()
 public class LoginAPI {
 
